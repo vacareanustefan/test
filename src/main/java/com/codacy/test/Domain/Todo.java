9b7fb003bbbabbc6d8dd48da1d637f57b7ead549
vacareanustefan <vacareanu.stefan07@gmail.com>
2021-09-02 12:01:56 +0300

    domain

---
+++ src/main/java/com/codacy/test/Domain/Todo.java
@@ -0,0 +1,6 @@
+package com.codacy.test.Domain;
+
+public class Todo {
+    private String title;
+    private boolean completed;
+}
---
+++ src/main/java/com/codacy/test/Domain/User.java
@@ -0,0 +1,8 @@
+package com.codacy.test.Domain;
+
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<Todo> todos;
+}
