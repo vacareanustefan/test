6ee673c5ac6c2cc1c61b9facc1b8b9b69ba075e0
vacareanustefan <vacareanu.stefan07@gmail.com>
2021-09-13 12:20:33 +0300

    creating coverage tests

--- src/test/java/com/codacy/test/TestApplicationTests.java
+++ src/test/java/com/codacy/test/TestApplicationTests.java
@@ -1,13 +1,48 @@
 package com.codacy.test;
 
+import com.codacy.test.Domain.Todo;
+import com.codacy.test.Domain.User;
+import com.codacy.test.Repository.TodoRepository;
+import com.codacy.test.Repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class TestApplicationTests {
 
+	@Autowired
+	private TodoRepository todoRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testSave() {
+		User user = new User("john");
+		userRepository.save(user);
+		Todo t1 = new Todo("this is a test", true, user);
+		Todo saveTodo = todoRepository.save(t1);
+
+		assertEquals(t1, saveTodo);
+	}
+
+	@Test
+	public void testUpdate() {
+		User user = new User("cris");
+		userRepository.save(user);
+		Todo todo = new Todo("test3", false, user);
+		Todo saveTodo = todoRepository.save(todo);
+		saveTodo.setTitle("I made an update");
+		Todo updateTodo = todoRepository.save(saveTodo);
+
+		assertEquals("I made an update", updateTodo.getTitle());
 	}
 
 }
